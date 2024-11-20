53cbe585895d82c817b41d31f422c15ed6085dab
ThetaHalo <theta@eps.lol>
2024-11-20 09:13:11 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+## Endpoint Debugger
+
+Simple [**Express.js**](https://expressjs.com/) app which logs all headers sent by a client.
+- If you use this a lot, consider giving me a star! ⭐
+
+
+### Running this.
+Run `npm i` if you haven't already, then type `npm start` or `npm run start`
+
+#### Hosting
+If you have your own domain, I recommend learning how to use [**Cloudflared**](https://github.com/cloudflare/cloudflared) and forwarding a port from your computer through it.
+
+If you don't have your own domain, and are unable to port forward normally, use a host. You can find some hosting providers with free tiers [**here.**](https://gist.github.com/bmaupin/d2d243218863320b01b0c1e1ca0cf5f3)
---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+
+const port = 4321; // change this to whatever, defaulted to 4321
+
+// adds a timestamp to the beginning of all requests.
+const originalLog = console.log;
+console.log = function (...args) {
+    const formattedDate = new Date().toLocaleString();
+    originalLog(`\x1b[35m[${formattedDate}]\x1b[0m`, ...args);
+};
+
+const originalErrorLog = console.error;
+console.error = function (...args) {
+    const formattedDate = new Date().toLocaleString();
+    originalErrorLog(`\x1b[35m[${formattedDate}]\x1b[0m`, ...args);
+};
+//
+
+app.use(express.json());
+app.use((req, res, next) => {
+
+    // This is only needed if your requests are forwarded through Cloudflare.
+    delete req.headers['x-forwarded-for']; // Remove IP forwarded by Cloudflare -> Optional
+    delete req.headers['cf-connecting-ip']; // Remove IP forwarded by Cloudflare -> Optional
+    delete req.headers['cf-warp-tag-id']; // Remove Data sent by Cloudflare -> Optional
+    delete req.headers['cf-ray']; // Remove Data sent by Cloudflare -> Optional
+
+    console.log(`Request received on route: ${req.path} | ${req.method}`);
+    console.log('Request Headers:', req.headers);
+    console.log('Request Body:', req.body);
+
+    next(); // add optional endpoints below this if you want to test a specific one.
+});
+
+
+app.listen(port, () => {
+    console.log(`\x1b[35mDebugger ready to go on port ${port}!\x1b[0m`);
+});
